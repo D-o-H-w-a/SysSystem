e5a902b5c8f59a821eeecac97d6a4468bb921996
DoHwa <wollyang583@naver.com>
2023-12-09 10:35:17 +0900

    중간 저장

--- Press_DB/Form1.cs
+++ Press_DB/Form1.cs
@@ -1,9 +1,17 @@
+using System;
+using System.Threading;
+using System.Windows.Forms;
+using OPCAutomation;
+
 namespace Press_DB
 {
     public partial class Form1 : Form
     {
-        // GroupBox1: textbox => Host: [수정x], textbox => HostIP [수정o], Button => Connect, Button => DisConnect, textbox => Status
-        // GroupBox2 : ListView[수정x] => DataView
+        private OPCServer opcServer; // 클라이언트와 서버간의 통신을 위해서 필요한 것들을 가져올 interface 변수.
+        private OPCGroup opcGround; // 서버에 저장된 OPCGroup을 클라이언트에 담아줄 interface 변수.
+        private Thread opcThread; // 
+        private bool isRunning;
+
         public Form1()
         {
             InitializeComponent();
