f62444702800c53b1e5a9cd514252a1f08eee3e7
DoHwa <wollyang583@naver.com>
2023-12-15 09:35:56 +0900

    작업 업로드

--- Press_DB/Form1.cs
+++ Press_DB/Form1.cs
@@ -11,17 +11,25 @@ namespace Press_DB
 {
     public partial class Form1 : Form
     {
-        // OPC 관련 변수 및 객체 선언
+        // OPC 서버 인스턴스를 나타내는 변수 선언
         private OPCServer opcServer;
+        // OPC 아이템들을 관리하는 객체 선언
         private OPCItems opcItems;
+        // OPC 아이템들을 담을 리스트 선언
         private List<OPCItem> opcItemList = new List<OPCItem>();
+        // OPC 그룹들을 관리하는 객체 선언
         private OPCGroups opcGroups;
+        // OPC 그룹을 나타내는 변수 선언
         private OPCGroup opcGroup;
+        // OPC 아이템을 나타내는 변수 선언
         private OPCItem opcItem;
+        // OPC 통신을 위한 스레드 변수 선언 
+        private Thread opcThread;
 
         public Form1()
         {
             InitializeComponent();
+            // 스레드 시작 메소드 호출
             StartThread();
         }
 
@@ -33,17 +41,22 @@ namespace Press_DB
         {
             // OPC 서버 연결
             opcServer = new OPCServer();
+            // OPC 서버에 연결
             opcServer.Connect("OPCServerIP");
 
-            // OPC 그룹 생성
-            opcGroups = opcServer.OPCGroups;
+            // OPC 그룹 생성 및 설정
+            opcGroups = opcServer.OPCGroups; // opc 서버에서 그룹을 관리하는 객체를 가져옴
+            // 이름에 맞는 OPC 그룹을 생성
             opcGroup = opcGroups.Add("YourGroup");
+            // OPC 그룹을 활성화
             opcGroup.IsActive = true;
+            // OPC 그룹을 구독 모드로 설정하여 실시간 데이터 수집
             opcGroup.IsSubscribed = true;
+            // OPC 아이템들을 관리하는 객체를 가져옴
             opcItems = opcGroup.OPCItems;
 
 
-            // OPC 아이템 추가 (예시)
+            // OPC 아이템 추가
             opcItemList.Add(opcItems.AddItem("PLT_IN_OUT", 1));
             opcItemList.Add(opcItems.AddItem("Job_Line", 1));
             opcItemList.Add(opcItems.AddItem("Serial_No", 1));
@@ -56,50 +69,56 @@ namespace Press_DB
             opcItemList.Add(opcItems.AddItem("Parts_count_int_pallet", 1));
             opcItemList.Add(opcItems.AddItem("Counts", 1));
 
-            // 필요한 나머지 OPC 아이템 추가
-
             // 데이터베이스 연결
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
+                // 데이터 베이스 연결을 열어옴
                 connection.Open();
 
                 // t_Cell 테이블에서 Cell_type 및 State 가져오기
                 SqlCommand cmdCell = new SqlCommand("SELECT Cell_type, State FROM t_Cell", connection);
+                // 셀 정보를 가져오기 위한 쿼리 실행
                 SqlDataReader readerCell = cmdCell.ExecuteReader();
 
+                // t_Cell 테이블 레코드 반복
                 while (readerCell.Read())
                 {
+                    // Cell_type 의 값을 가져옴
                     int cellType = Convert.ToInt32(readerCell["Cell_type"]);
+                    // State 의 값을 가져옴
                     string state = readerCell["State"].ToString();
 
                     // t_SC_state 테이블에서 Stk_state 값 가져오기
                     SqlCommand cmdSCState = new SqlCommand("SELECT Stk_state FROM t_SC_state", connection);
+                    // SC_state 정보를 가져오기 위한 쿼리 실행
                     SqlDataReader readerSCState = cmdSCState.ExecuteReader();
 
+                    // t_SC_state 테이블 레코드 반복
                     while (readerSCState.Read())
                     {
+                        // Stk_state 의 값을 가져옴
                         int stkState = Convert.ToInt32(readerSCState["Stk_state"]);
-                        
+                        //OPC 아이템 값 가져오기
                         int value = Convert.ToInt32(opcItemList.Find(item => item.ItemID == "Item"));
                         // 조건 검사 및 처리
-                        // opcItemList 리스트의 원하는 인덱스를 사용하여 OPC 아이템 값에 접근하여 조건 검사
+                        // OPC 아이템 값과 데이터베이스에서 가져온 값들을 조건으로 검사
                         if (value < cellType && state == "Empty" && stkState == 0)
                         {
-                            // In_reserve 값 저장
-                            // 이 부분에서 In_reserve 값을 저장하는 방법에 따라 적절한 처리를 추가하세요.
-                            // 예: SqlCommand를 사용하여 데이터베이스에 값을 삽입하는 방법 등
+                            // 조건에 맞을 경우 데이터베이스에 삽입
+                            InsertToDatabase(connection);
                         }
                     }
-
+                    // t_SC_state 리더 닫기
                     readerSCState.Close();
                 }
 
+                // t_Cell 리더 닫기
                 readerCell.Close();
+                // 데이터베이스 연결 닫기
                 connection.Close();
+                // 스레드 종료
+                StopThread();
             }
-
-            // 스레드 종료
-            // 필요한 경우 스레드 종료 처리 추가
         }
 
         private void InsertToDatabase(SqlConnection connection)
@@ -139,8 +158,22 @@ namespace Press_DB
         // 스레드 시작부분
         private void StartThread()
         {
-            Thread opcThread = new Thread(new ThreadStart(ConnectToOPCServer));
+            // ConnectToOPCServer 메서드를 실행하기 위한 새로운 스레드를 생성
+            opcThread = new Thread(new ThreadStart(ConnectToOPCServer));
+            // 생성한 스레드를 시작
             opcThread.Start();
         }
+
+        // 스레드 종료 메서드
+        private void StopThread()
+        {
+            // opcThread 값이 null 아니며 opcThread 가 동작중일 시
+            if (opcThread != null && opcThread.IsAlive)
+            { 
+                // 스레드 종료 요청
+                opcThread.Abort();
+            }
+        }
+
     }
 }
